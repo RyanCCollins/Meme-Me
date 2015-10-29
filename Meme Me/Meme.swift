68219f81eb619da769e11db47ff9bc6e37c5451e
techrapport <info@techrapport.com>
2015-10-29 17:35:58 -0400

    Change Meme from class to struct.

--- Meme Me/Meme.swift
+++ Meme Me/Meme.swift
@@ -14,81 +14,105 @@ struct Meme {
     var originalImage: UIImage!
     var memedImage: UIImage!
     var fontAttributes: FontAttributes!
+}
+
+extension Meme {
+    static var allMemes: [Meme] {
+        return getMemeStorage().memes
+    }
+
     
-//    var fontSize: CGFloat!
-//    var fontColor: UIColor!
-//    var borderColor: UIColor!
-    
-    init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage, fontAttributes: FontAttributes) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-        
-        //Define font attributes:
-        self.fontAttributes = fontAttributes
+    static func add(meme: Meme) {
+        getMemeStorage().memes.append(meme)
     }
     
-    //Function to save the meme:
-    func saveMeme() {
-        Meme.getMemeStorage().memes.append(self)
+    static func updateMeme(atIndex index: Int, withMeme meme: Meme) {
+        getMemeStorage().memes[index] = meme
     }
     
-    //Function to update an existing meme:
-    mutating func updateMeme(topText: String, bottomText: String, originalImage:UIImage, memedImage: UIImage, fontAttributes: FontAttributes) {
-        self.topText = topText
-        self.bottomText = bottomText
-        self.originalImage = originalImage
-        self.memedImage = memedImage
-        
-        self.fontAttributes = fontAttributes
+    static func remove(atIndex index: Int) {
+        getMemeStorage().memes.removeAtIndex(index)
     }
     
-    //Update font traits:
-    mutating func changeFont(fontSize: CGFloat, fontColor: UIColor) {
-        fontAttributes.fontColor = fontColor
-        fontAttributes.fontSize = fontSize
-    }
-    
-    //Collect all memes into an array:
-    static func getAll() -> [Meme] {
-        return Meme.getMemeStorage().memes
-    }
-    
-    //Return the location of our appdelegate meme object:
+    //Locate the meme storage location
     static func getMemeStorage() -> AppDelegate {
         let object = UIApplication.sharedApplication().delegate
         return object as! AppDelegate
     }
-    
-    static func countMemes() -> Int {
-        return Meme.getMemeStorage().memes.count
-    }
-    
-    static func getMemeAtIndex(index: Int) -> Meme? {
-        if Meme.getMemeStorage().memes.count > index {
-            return Meme.getMemeStorage().memes[index]
-        }
-        return nil
-    }
-    
-    static func removeMemeAtIndex(index: Int) {
-        if index >= 0 {
-            Meme.getMemeStorage().memes.removeAtIndex(index)
-        }
-    }
 }
 
-struct FontAttributes {
-    var fontSize: CGFloat!
-    var fontName: String!
-    var fontColor: UIColor
-    var borderColor: UIColor!
+////    init(topText: String, bottomText: String, originalImage: UIImage, memedImage: UIImage, fontAttributes: FontAttributes) {
+////        self.topText = topText
+////        self.bottomText = bottomText
+////        self.originalImage = originalImage
+////        self.memedImage = memedImage
+////        
+////        //Define font attributes:
+////        self.fontAttributes = fontAttributes
+////    }
+//    
+//    //Function to save the meme:
+//    func saveMeme() {
+//        Meme.getMemeStorage().memes.append(self)
+//    }
+//    
+//    //Function to update an existing meme:
+//    mutating func updateMeme(atIndex index: Int, topText: String, bottomText: String, originalImage:UIImage, memedImage: UIImage, fontAttributes: FontAttributes) {
+//        var editMeme
+//        self.topText = topText
+//        self.bottomText = bottomText
+//        self.originalImage = originalImage
+//        self.memedImage = memedImage
+//        
+//        self.fontAttributes = fontAttributes
+//    }
+//    
+//    //Update font traits:
+//    mutating func changeMemeFont(atIndex index: Int) {
+//        let meme = getMemeAtIndex(index)
+//        meme.fontColor = fontColor
+//        fontAttributes.fontSize = fontSize
+//    }
+//    
+//    //Collect all memes into an array:
+//    static func getAll() -> [Meme] {
+//        return Meme.getMemeStorage().memes
+//    }
+//    
+//    //Return the location of our appdelegate meme object:
+//    static func getMemeStorage() -> AppDelegate {
+//        let object = UIApplication.sharedApplication().delegate
+//        return object as! AppDelegate
+//    }
+//    
+//    static func countMemes() -> Int {
+//        return getAll().count
+//    }
+//    
+//    static func getMemeAtIndex(index: Int) -> Meme? {
+//        if Meme.getMemeStorage().memes.count > index {
+//            return Meme.getMemeStorage().memes[index]
+//        }
+//        return nil
+//    }
+//    
+//    static func removeMemeAtIndex(index: Int) {
+//        if index >= 0 {
+//            Meme.getMemeStorage().memes.removeAtIndex(index)
+//        }
+//    }
+//}
 
-    init(fontSize: CGFloat = 40.0, fontName: String = "HelveticaNeue-CondensedBlack", fontColor: UIColor = UIColor.whiteColor(), borderColor: UIColor = UIColor.blackColor()) {
-        self.fontSize = fontSize
-        self.fontName = fontName
-        self.fontColor = fontColor
-        self.borderColor = borderColor
-    }
+struct FontAttributes {
+    var fontSize: CGFloat = 40.0
+    var fontName = "HelveticaNeue-CondensedBlack"
+    var fontColor = UIColor.whiteColor()
+    var borderColor = UIColor.blackColor()
+
+//    init(fontSize: CGFloat = 40.0, fontName: String = "HelveticaNeue-CondensedBlack", fontColor: UIColor = UIColor.whiteColor(), borderColor: UIColor = UIColor.blackColor()) {
+//        self.fontSize = fontSize
+//        self.fontName = fontName
+//        self.fontColor = fontColor
+//        self.borderColor = borderColor
+//    }
 }
